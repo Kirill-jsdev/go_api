57ae00c0a4f3cd0ae3273ead8fd6d6f0734b7acf
Kirill Zlobin <jsdevzk@gmail.com>
2025-04-19 14:38:34 +0500

    Sent first JSON from the backend

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world from %s", app.Domain)
+	var payload = struct {
+        Status  string `json:"status"`
+        Message string `json:"message"`
+        Version string `json:"version"`
+    }{
+        Status:  "success",
+        Message: "Welcome to the API",
+        Version: "1.0.0",
+    }
+
+	//convert to json
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonData)
 }
