4aac811fb0a86b66f8a81e96f42109cff5092202
Kirill Zlobin <jsdevzk@gmail.com>
2025-04-19 15:46:46 +0500

    Added /movies rout handler

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"backend/internal/models"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +21,48 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 
 	//convert to json
 	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonData)
+}
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+	var movies []models.Movie
+
+	releaseDate, _ := time.Parse("2006-01-02", "1986-03-07")
+	highlander := models.Movie {
+		ID: 1,
+		Title: "Highlander",
+		ReleaseDate: releaseDate,
+		MPAARating: "R",
+		Runtime: 116,
+		Description: "Nice movie",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	movies = append(movies, highlander)
+
+	releaseDate, _ = time.Parse("2006-01-02", "1986-03-07")
+	batman := models.Movie {
+		ID: 2,
+		Title: "Batman",
+		ReleaseDate: releaseDate,
+		MPAARating: "R",
+		Runtime: 116,
+		Description: "Nice movie too",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	movies = append(movies, batman)
+
+	//convert to json
+	jsonData, err := json.Marshal(movies)
 	if err != nil {
 		fmt.Println(err)
 	}
