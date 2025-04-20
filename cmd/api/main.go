969b8f50a53073cfdda21385c267937a7400d8a1
Kirill Zlobin <jsdevzk@gmail.com>
2025-04-20 19:14:39 +0500

    Connected to DB

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -12,6 +13,7 @@ const port = 8080
 type application struct{
 	DSN string
 	Domain string
+	DB *sql.DB
 }
 
 func main() {
@@ -21,13 +23,21 @@ func main() {
 	//read from command line
 	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
 	flag.Parse()
+
 	//connect to the db
+	conn, err := app.connectToDB()
+	if err != nil {
+        log.Fatal(err)
+    }
+
+	app.DB = conn
+	defer app.DB.Close()
 
 	app.Domain = "example.com"
 	
 	//start a web server
 	log.Println("Started app on port", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 
 	if err != nil {
 		log.Fatal(err)
